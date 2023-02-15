49e5c0cffb153de502dece54ecb331b99d764a5c
FaribaZ <111224004+FaribaZ@users.noreply.github.com>
2023-02-15 18:03:58 +0330

    added forecast for one day

---
+++ src/WeatherForcast.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./WeatherForcast.css";
+import WeatherForecastDay from "./WeatherForecastDay";
+
+export default function WeatherForcast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+  if (loaded) {
+    return (
+      <div>
+        <div className="row">
+          <div className="col"></div>
+          <WeatherForecastDay data={forecast[0]} />
+        </div>
+      </div>
+    );
+  } else {
+    let apiKey = "f8e6a9e3d6fde87cb38868da460b1371";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+  }
+}
---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function WeatherForecastDay(props) {
+  function maxTemp() {
+    let temp = Math.round(props.data.temp.max);
+    return `${temp}°`;
+  }
+  function minTemp() {
+    let temp = Math.round(props.data.temp.min);
+    return `${temp}°`;
+  }
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[day];
+  }
+  return (
+    <div>
+      <div className="forecast">
+        <div className="d-flex justify-content-evenly">
+          <div className="days">
+            <div className="forecastDay">{day()}</div>
+            <div className="forecastIcon">
+              <img src={props.data.weather[0].icon} alt="weather icon" />
+            </div>
+            <div className="weatherTemp">
+              <span className="maxTemp">{maxTemp()}</span>
+              <span className="minTemp">{minTemp()}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
